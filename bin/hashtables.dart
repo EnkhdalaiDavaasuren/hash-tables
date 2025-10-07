d2d6f6420705755466238d8bbaafe26c5d775dea
EnkhdalaiDavaasuren <2024012@miu.edu.mn>
2025-10-07 12:28:12 +0800

    added binarySearch

--- bin/hashtables.dart
+++ bin/hashtables.dart
@@ -1,7 +1,13 @@
+import 'package:hashtables/binary_search.dart';
 import 'package:hashtables/linear_search.dart';
 
 void main() {
   final myList = [42, 3, 10, 7, 28, 149, 0, 3, 25, 7, 1];
-  final foundIndex = linearSearch(myList, 28);
+  var foundIndex = linearSearch(myList, 10);
+
+  myList.sort();
+  print(myList);
+
+  foundIndex = binarySearch(myList, 1);
   print(foundIndex);
 }
